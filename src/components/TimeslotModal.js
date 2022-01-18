7558a43c53c6ab913219cb741d103ef34444bd1e
Luke Gabrieli <lukezgabrieli@gmail.com>
2022-01-17 18:44:09 -0800

    modal styles

--- src/components/TimeslotModal.js
+++ src/components/TimeslotModal.js
@@ -2,7 +2,53 @@ import React, {useState, useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 import moment from 'moment';
 import ReactModal from 'react-modal';
+import styled from 'styled-components';
 import {addTimeslot, editTimeslot} from '../redux/timeslot/timeslotSlice';
+
+const StyledContainer = styled.div`
+	position: relative;
+`;
+
+const StyledCloseButton = styled.button`
+	position: absolute;
+	top: 0;
+	right: 0;
+	margin: 0;
+	padding: 8px;
+	background-color: transparent;
+	border: none;
+	cursor: pointer;
+	font-size: 24px;
+`;
+
+const StyledContent = styled.div`
+	padding-top: 44px;
+`;
+
+const StyledInputRow = styled.div`
+	margin-bottom: 12px;
+`;
+
+const StyledLabel = styled.label`
+	margin-right: 12px;
+`;
+
+const StyledErrorMessage = styled.p`
+	margin: 0;
+	margin-top: 2px;
+	padding: 0;
+	color: #bb2020;
+	font-size: 14px;
+`;
+
+const StyledButton = styled.button`
+	margin: 0;
+	padding: 12px 24px;
+	background-color: #3eb595;
+	border-radius: 12px;
+	border: none;
+	cursor: pointer;
+`;
 
 ReactModal.setAppElement('#root');
 
@@ -106,102 +152,121 @@ function TimeslotModal({isOpen = false, onClose, timeslot}) {
 	};
 
 	return (
-		<>
-			<ReactModal isOpen={isOpen} onRequestClose={onClose} contentLabel='Modal'>
-				<button
+		<ReactModal isOpen={isOpen} onRequestClose={onClose} contentLabel='Modal'>
+			<StyledContainer>
+				<StyledCloseButton
 					onClick={() => {
+						if (!timeslot) {
+							// reset form fields
+							setActivityName('');
+							setDate('');
+							setStartTime('');
+							setEndTime('');
+							setNumMaxGuests(0);
+							// reset errors
+							setActivityNameError(false);
+							setDateError(false);
+							setStartTimeError(false);
+							setEndTimeError(false);
+							setNumMaxGuestsError(false);
+						}
 						// call onClose
 						onClose();
 					}}
 				>
-					Close modal
-				</button>
-				<form onSubmit={handleSubmit}>
-					<div>
-						<label htmlFor='activity-name'>Activity Name:</label>
-						<input
-							id='activity-name'
-							name='activity-name'
-							type='text'
-							value={activityName}
-							onChange={(e) => {
-								if (activityNameError) {
-									setActivityNameError(false);
-								}
-								setActivityName(e.target.value);
-							}}
-						/>
-						{activityNameError ? <p>Activity name is a required field</p> : null}
-					</div>
-					<div>
-						<label htmlFor='date'>Date:</label>
-						<input
-							id='date'
-							name='date'
-							type='date'
-							min={moment().format('YYYY-MM-DD')}
-							value={date}
-							onChange={(e) => {
-								if (dateError) {
-									setDateError(false);
-								}
-								setDate(e.target.value);
-							}}
-						/>
-						{dateError ? <p>Date is a required field</p> : null}
-					</div>
-					<div>
-						<label htmlFor='start-time'>Start Time:</label>
-						<input
-							id='start-time'
-							name='start-time'
-							type='time'
-							value={startTime}
-							onChange={(e) => {
-								if (startTimeError) {
-									setStartTimeError(false);
-								}
-								setStartTime(e.target.value);
-							}}
-						/>
-						{startTimeError ? <p>Start time is a required field</p> : null}
-					</div>
-					<div>
-						<label htmlFor='end-time'>End Time:</label>
-						<input
-							id='end-time'
-							name='end-time'
-							type='time'
-							value={endTime}
-							onChange={(e) => {
-								if (endTimeError) {
-									setEndTimeError(false);
-								}
-								setEndTime(e.target.value);
-							}}
-						/>
-						{endTimeError ? <p>End time is a required field</p> : null}
-					</div>
-					<div>
-						<label htmlFor='num-max-guests'>Maximum number of guests:</label>
-						<input
-							id='num-max-guests'
-							name='num-max-guests'
-							type='number'
-							value={numMaxGuests}
-							onChange={(e) => {
-								if (numMaxGuestsError) {
-									setNumMaxGuestsError(false);
-								}
-								setNumMaxGuests(e.target.value);
-							}}
-						/>
-						{numMaxGuestsError ? <p>Maximum number of guests must be greater than zero</p> : null}
-					</div>
-					<button>{timeslot ? 'Edit timeslot' : 'Add timeslot'}</button>
-				</form>
-			</ReactModal>
-		</>
+					✕
+				</StyledCloseButton>
+				<StyledContent>
+					<h2>{timeslot ? 'Edit timeslot' : 'Add timeslot'}</h2>
+					<form onSubmit={handleSubmit}>
+						<StyledInputRow>
+							<StyledLabel htmlFor='activity-name'>Activity Name:</StyledLabel>
+							<input
+								id='activity-name'
+								name='activity-name'
+								type='text'
+								value={activityName}
+								onChange={(e) => {
+									if (activityNameError) {
+										setActivityNameError(false);
+									}
+									setActivityName(e.target.value);
+								}}
+							/>
+							{activityNameError ? <StyledErrorMessage>Activity name is a required field</StyledErrorMessage> : null}
+						</StyledInputRow>
+						<StyledInputRow>
+							<StyledLabel htmlFor='date'>Date:</StyledLabel>
+							<input
+								id='date'
+								name='date'
+								type='date'
+								min={moment().format('YYYY-MM-DD')}
+								value={date}
+								onChange={(e) => {
+									if (dateError) {
+										setDateError(false);
+									}
+									setDate(e.target.value);
+								}}
+							/>
+							{dateError ? <StyledErrorMessage>Date is a required field</StyledErrorMessage> : null}
+						</StyledInputRow>
+						<StyledInputRow>
+							<StyledLabel htmlFor='start-time'>Start Time:</StyledLabel>
+							<input
+								id='start-time'
+								name='start-time'
+								type='time'
+								value={startTime}
+								onChange={(e) => {
+									if (startTimeError) {
+										setStartTimeError(false);
+									}
+									setStartTime(e.target.value);
+								}}
+							/>
+							{startTimeError ? <StyledErrorMessage>Start time is a required field</StyledErrorMessage> : null}
+						</StyledInputRow>
+						<StyledInputRow>
+							<StyledLabel htmlFor='end-time'>End Time:</StyledLabel>
+							<input
+								id='end-time'
+								name='end-time'
+								type='time'
+								value={endTime}
+								onChange={(e) => {
+									if (endTimeError) {
+										setEndTimeError(false);
+									}
+									setEndTime(e.target.value);
+								}}
+							/>
+							{endTimeError ? <StyledErrorMessage>End time is a required field</StyledErrorMessage> : null}
+						</StyledInputRow>
+						<StyledInputRow>
+							<StyledLabel htmlFor='num-max-guests'>Maximum number of guests:</StyledLabel>
+							<input
+								id='num-max-guests'
+								name='num-max-guests'
+								type='number'
+								value={numMaxGuests}
+								onChange={(e) => {
+									if (numMaxGuestsError) {
+										setNumMaxGuestsError(false);
+									}
+									setNumMaxGuests(e.target.value);
+								}}
+							/>
+							{numMaxGuestsError ? (
+								<StyledErrorMessage>Maximum number of guests must be greater than zero</StyledErrorMessage>
+							) : null}
+						</StyledInputRow>
+						<StyledButton>Submit</StyledButton>
+					</form>
+				</StyledContent>
+			</StyledContainer>
+		</ReactModal>
 	);
 }
 
